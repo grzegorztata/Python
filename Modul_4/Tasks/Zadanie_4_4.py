15f7afc54263913b1c24a7a727f854311c9b0666
Grzegorz Tąta <GTata@sygnity.pl>
2023-05-04 20:03:15 +0200

    calculator fix

--- Modul_4/Tasks/Zadanie_4_4.py
+++ Modul_4/Tasks/Zadanie_4_4.py
@@ -2,49 +2,48 @@ import sys
 import logging
 logging.basicConfig(level=logging.INFO)
 
-def add(a, b):
-    result = a + b
-    logging.info(f"Dodaję liczby {a} i {b}. Wynik: {result}")
-    print(result)
+def add(*args):
+    result = sum(args)
+    logging.info(f"Dodaję liczby {args}. Wynik {result}")
     
-def minus(a, b):
+def subs(a, b):
     result = a - b
     logging.info(f"Odejmuję liczbę {b} od {a}. Wynik {result}")
-    print(result)
     
-def x_times(a, b):
-    result = a * b
-    logging.info(f"Mnożę liczby {a} i {b}. Wynik {result}")
-    print(result)
+def multi(*args):
+    result = 1
+    for x in args:
+        result *= x
+    logging.info(f"Mnożę liczby {args}. Wynik {result}")
     
 def divide(a, b):
-    logging.info(f"Dzielę liczby {a} przez {b}")
     if b == 0:
         logging.warning("Dzielenie przez 0 jest niedozwolone")
-        return
     else:
-        return a / b
+        result = a / b
+        logging.info(f"Dzielę liczbę {a} przez {b}. Wynik {result}")
 
-def wrong_task():  # na spotkanie
+def wrong_task():
     logging.warning("Wybierz poprawne działanie")
     sys.exit()
 
 if __name__ == "__main__":
     task = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
-    a = int(input("Podaj pierwszą liczbę: "))
-    b = int(input("Podaj drugą liczbę: "))
+    
+    if task in [1, 3]:
+        nums = input("Podaj liczby oddzielone spacjami: ")
+        nums_list = [int(num.strip()) for num in nums.split(" ")]
+    elif task in [2, 4]:
+        a = int(input("Podaj pierwszą liczbę: "))
+        b = int(input("Podaj drugą liczbę: "))
     
     if task == 1:
-        result = add(a, b)
-        print(result)
+        result = add(*nums_list)
     elif task == 2:
-        result = minus(a, b)
-        print(result)
+        result = subs(a, b)
     elif task == 3:
-        result = x_times(a, b)
-        print(result)
+        result = multi(*nums_list)
     elif task == 4:
         result = divide(a, b)
-        print(result)
     else:
-        wrong_task()  # na spotkanie
+        wrong_task()
