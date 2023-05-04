5b00ac6a050b3e71fc098dfac25c4a136307a601
Grzegorz Tąta <GTata@sygnity.pl>
2023-05-04 20:15:32 +0200

    Zadanie_modul_4_4_add_init_function

--- Modul_4/Tasks/Zadanie_4_4.py
+++ Modul_4/Tasks/Zadanie_4_4.py
@@ -26,24 +26,30 @@ def divide(a, b):
 def wrong_task():
     logging.warning("Wybierz poprawne działanie")
     sys.exit()
-
-if __name__ == "__main__":
+    
+def init_app():
     task = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
     
     if task in [1, 3]:
         nums = input("Podaj liczby oddzielone spacjami: ")
         nums_list = [int(num.strip()) for num in nums.split(" ")]
+        return task, nums_list
     elif task in [2, 4]:
         a = int(input("Podaj pierwszą liczbę: "))
         b = int(input("Podaj drugą liczbę: "))
-    
-    if task == 1:
-        result = add(*nums_list)
-    elif task == 2:
-        result = subs(a, b)
-    elif task == 3:
-        result = multi(*nums_list)
-    elif task == 4:
-        result = divide(a, b)
+        return task, a,b
     else:
-        wrong_task()
+        print("Niepoprawna wartość. Spróbuj jeszcze raz")
+        sys.exit()
+
+if __name__ == "__main__":
+    task_input = init_app()
+    if task_input[0] == 1:
+        result = add(*task_input[1])
+    elif task_input[0] == 2:
+        result = subs(*task_input[1:])
+    elif task_input[0] == 3:
+        result = multi(*task_input[1])
+    elif task_input[0] == 4:
+        result = divide(*task_input[1:])
+        
